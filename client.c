9682862580468455ff9900a68ff917dcd240e58e
likun <likun@bigo.sg>
2023-06-15 15:01:43 +0800

    multiple client severt

--- client.c
+++ client.c
@@ -5,6 +5,7 @@
 #include<sys/types.h>
 #include<sys/socket.h>
 #include<netinet/in.h>
+#include<pthread.h>
 #if __APPLE__
 #include <unistd.h>
 #include <arpa/inet.h>
@@ -104,19 +105,32 @@ int send_gps_data();
 int send_gsensor_data();
 int send_gyroscope_data();
 
+int gps_thread();
+int imu_thread();
+
 int main(int argc, char** argv) {
+    pthread_t imu_pid;
+    pthread_t gps_pid;
+    pthread_create(&imu_pid, NULL, imu_thread, NULL);
+    pthread_create(&gps_pid, NULL, gps_thread, NULL);
+
+    pthread_join(imu_pid, NULL);
+    pthread_join(gps_pid, NULL);
+    return 0;
+}
+
+int gps_thread() {
     // char address[] = "172.23.168.149";
     // char address[] = "192.168.158.100";
-    char address[] = "192.168.1.200";
+    char address[] = "127.0.0.1";
     int    sockfd, n;
     char    recvline[MAXLINE], sendline[MAXLINE];
     struct sockaddr_in    servaddr;
-
+    SOCKET_MESSAGE socket_gps;
     if( (sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         printf("create socket error: %s(errno: %d)\n", strerror(errno),errno);
         exit(0);
     }
-
     memset(&servaddr, 0, sizeof(servaddr));
     servaddr.sin_family = AF_INET;
     servaddr.sin_port = htons(12345);
@@ -124,58 +138,29 @@ int main(int argc, char** argv) {
         printf("inet_pton error for %s\n", address);
         exit(0);
     }
-
     if( connect(sockfd, (struct sockaddr*)&servaddr, sizeof(servaddr)) < 0){
         printf("connect error: %s(errno: %d)\n",strerror(errno),errno);
         exit(0);
     }
-
-
-    printf("gyro_data size: %d\n", sizeof(gyro_data));
-    printf("gsensor_data size: %d\n", sizeof(gsensor_data));
     printf("gps type size: %d\n", sizeof(FW_GPS_INFO_STRU));
     printf("gps dat size: %d\n", sizeof(gps_data));
     printf("gps dat size: %d\n", sizeof(GPS_INFO_PACKED));
-
     int i = 0;
     while(1) {
         gps_data.azimuth = i;
-        int buf_len = send_gps_data();
-        printf("gps send len: %d %d\n", buf_len, i);
-        for(int i = 0; i < buf_len; ++i) {
-            printf("%02x", socket_message.data[i]);
-        }
-        printf("\n");
-        if( send(sockfd, socket_message.data, buf_len, 0) < 0) {
-            printf("send gyro msg error: %s(errno: %d)\n", strerror(errno), errno);
-            exit(0);
-        }
-        gsensor_data.acc_x = i;
-        buf_len = send_gsensor_data();
-        printf("gsensor send len: %d %d\n", buf_len, i);
-        for(int i = 0; i < buf_len; ++i) {
-            printf("%02x", socket_message.data[i]);
-        }
-        printf("\n");
-        if( send(sockfd, socket_message.data, buf_len, 0) < 0) {
-            printf("send gyro msg error: %s(errno: %d)\n", strerror(errno), errno);
-            exit(0);
-        }
-        gyro_data.gyro_x = i;
-        buf_len = send_gyroscope_data();
-        printf("gyro send len: %d %d\n", buf_len, i);
-        for(int i = 0; i < buf_len; ++i) {
-            printf("%02x", socket_message.data[i]);
-        }
-        printf("\n");
-        if( send(sockfd, socket_message.data, buf_len, 0) < 0) {
+        int buf_len = send_gps_data(&socket_gps);
+        // printf("gps: %d\n", buf_len);
+        // for(int k = 0; k < buf_len; ++k) {
+        //     printf("%hx", socket_gps.data[k]);
+        // }
+        // printf("\n");
+        if( send(sockfd, socket_gps.data, buf_len, 0) < 0) {
             printf("send gyro msg error: %s(errno: %d)\n", strerror(errno), errno);
             exit(0);
         }
         i++;
         usleep(1000*10);
     }
-
     memset(recvline, 0, MAXLINE);
     if (recv(sockfd, recvline, MAXLINE, 0) < 0) {
         printf("recv msg error: %s(errno: %d)\n", strerror(errno), errno);
@@ -188,31 +173,87 @@ int main(int argc, char** argv) {
     exit(0);
 }
 
-int send_gyroscope_data() {
+int imu_thread() {
+    char address[] = "127.0.0.1";
+    int    sockfd, n;
+    char    recvline[MAXLINE], sendline[MAXLINE];
+    struct sockaddr_in    servaddr;
+    SOCKET_MESSAGE socket_imu;
+    if( (sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        printf("create socket error: %s(errno: %d)\n", strerror(errno),errno);
+        exit(0);
+    }
+    memset(&servaddr, 0, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(12345);
+    if( inet_pton(AF_INET, address, &servaddr.sin_addr) <= 0){
+        printf("inet_pton error for %s\n", address);
+        exit(0);
+    }
+    if( connect(sockfd, (struct sockaddr*)&servaddr, sizeof(servaddr)) < 0){
+        printf("connect error: %s(errno: %d)\n",strerror(errno),errno);
+        exit(0);
+    }
+    printf("gyro_data size: %d\n", sizeof(gyro_data));
+    printf("gsensor_data size: %d\n", sizeof(gsensor_data));
+    int i = 0;
+    while(1) {
+        gsensor_data.acc_x = i;
+        int buf_len = send_gsensor_data(&socket_imu);
+        // printf("gsensor: ");
+        // for(int k = 0; k < buf_len; ++k) {
+        //     printf("\\x%hx", socket_imu.data[k]);
+        // }
+        // printf("\n");
+        if( send(sockfd, socket_imu.data, buf_len, 0) < 0) {
+            printf("send gyro msg error: %s(errno: %d)\n", strerror(errno), errno);
+            exit(0);
+        }
+        gyro_data.gyro_x = i;
+        buf_len = send_gyroscope_data(&socket_imu);
+        if( send(sockfd, socket_imu.data, buf_len, 0) < 0) {
+            printf("send gyro msg error: %s(errno: %d)\n", strerror(errno), errno);
+            exit(0);
+        }
+        i++;
+        usleep(1000*10);
+    }
+    memset(recvline, 0, MAXLINE);
+    if (recv(sockfd, recvline, MAXLINE, 0) < 0) {
+        printf("recv msg error: %s(errno: %d)\n", strerror(errno), errno);
+        exit(0);
+    } else {
+        printf("recv msg: %s\n", recvline);
+    }
+    close(sockfd);
+    exit(0);
+}
+
+int send_gyroscope_data(SOCKET_MESSAGE *socket_message) {
     int type = 0;
-    char *p_type = socket_message.data;
+    char *p_type = socket_message->data;
     char *p_data = p_type + sizeof(int);
     char *p_end = p_data + sizeof(gyro_data);
-    memset(socket_message.data, 0, sizeof(SOCKET_MESSAGE_LENGTH));
+    memset(socket_message->data, 0, SOCKET_MESSAGE_LENGTH);
     memcpy(p_type, &type, sizeof(int));
     memcpy(p_data, &gyro_data, sizeof(gyro_data));
-    memcpy(p_end, "\n\n\t\t", sizeof(4));
+    memcpy(p_end, "\n\n\t\t", 4);
     return sizeof(gyro_data) + 8;
 }
 
-int send_gsensor_data() {
+int send_gsensor_data(SOCKET_MESSAGE *socket_message) {
     int type = 1;
-    char *p_type = socket_message.data;
+    char *p_type = socket_message->data;
     char *p_data = p_type + sizeof(int);
     char *p_end = p_data + sizeof(gsensor_data);
-    memset(socket_message.data, 0, sizeof(SOCKET_MESSAGE_LENGTH));
+    memset(socket_message->data, 0, SOCKET_MESSAGE_LENGTH);
     memcpy(p_type, &type, sizeof(int));
     memcpy(p_data, &gsensor_data, sizeof(gsensor_data));
-    memcpy(p_end, "\n\n\t\t", sizeof(4));
+    memcpy(p_end, "\n\n\t\t", 4);
     return sizeof(gsensor_data) + 8;
 }
 
-int send_gps_data() {
+int send_gps_data(SOCKET_MESSAGE *socket_message) {
     gps_data_packed.azimuth = gps_data.azimuth;
     gps_data_packed.SNR = gps_data.SNR;
     gps_data_packed.accuracy = gps_data.accuracy; /*0|1-50 m*/
@@ -233,12 +274,12 @@ int send_gps_data() {
     gps_data_packed.pitch = gps_data.pitch;
     gps_data_packed.heading = gps_data.heading;
     int type = 2;
-    char *p_type = socket_message.data;
+    char *p_type = socket_message->data;
     char *p_data = p_type + sizeof(int);
     char *p_end = p_data + sizeof(gps_data_packed);
-    memset(socket_message.data, 0, sizeof(SOCKET_MESSAGE_LENGTH));
+    memset(socket_message->data, 0, SOCKET_MESSAGE_LENGTH);
     memcpy(p_type, &type, sizeof(int));
     memcpy(p_data, &gps_data_packed, sizeof(gps_data_packed));
-    memcpy(p_end, "\n\n\t\t", sizeof(4));
+    memcpy(p_end, "\n\n\t\t", 4);
     return sizeof(gps_data_packed) + 8;
 }
--- server.py
+++ server.py
@@ -1,49 +1,52 @@
 import socket
 import struct
+import _thread
+
+
+def on_new_client(clientsocket, addr):
+    print("connect addr: ", addr)
+    reserved_data = b''
+    while True:
+        client_data = clientsocket.recv(1024)
+        if len(client_data) == 0:
+            continue
+        client_data = reserved_data + client_data
+        reserved_data = b''
+        for msg_data in client_data.split(b'\n\n\t\t'):
+            if (len(msg_data) == 0):
+                continue
+            fmt = 'i%ds' % (len(msg_data)-4)
+            message_data = struct.unpack(fmt, msg_data)
+            print("message_data: type: ", message_data[0])
+            print("message_data: len: ", len(message_data[1]))
+            if message_data[0] == 0:
+                if (len(message_data[1]) != 16):
+                    reserved_data = msg_data
+                    continue
+                gyro_data = struct.unpack('4f', message_data[1])
+                print('gyro_data: ', gyro_data)
+            elif message_data[0] == 1:
+                if (len(message_data[1]) != 20):
+                    reserved_data = msg_data
+                    continue
+                gsensor_data = struct.unpack('5f', message_data[1])
+                print('gsensor_data: ', gsensor_data)
+            elif message_data[0] == 2:
+                if (len(message_data[1]) != 112):
+                    reserved_data = msg_data
+                    continue
+                gps_data = struct.unpack('=2i3f2d32s2f3i6f', message_data[1])
+                print('gps_data: ', gps_data)
+            else:
+                print("invalid socket data type")
+    #     clientsocket.send(b'server send data')
+    # clientsocket.close()
 
 s = socket.socket()
 port = 12345
-
 s.bind(('0.0.0.0', port))
-s.listen(1)
-c, addr = s.accept()
-print("connection address: ", addr)
-
-reserved_data = b''
+s.listen(2)
 while True:
-    client_data = c.recv(1024)
-    if len(client_data) == 0:
-        continue
-    client_data = reserved_data + client_data
-    reserved_data = b''
-    for msg_data in client_data.split(b'\n\n\t\t'):
-        if (len(msg_data) == 0):
-            continue
-        fmt = 'i%ds' % (len(msg_data)-4)
-        message_data = struct.unpack(fmt, msg_data)
-        print("message_data: type: ", message_data[0])
-        print("message_data: len: ", len(message_data[1]))
-        if message_data[0] == 0:
-            if (len(message_data[1]) != 16):
-                reserved_data = msg_data
-                continue
-            gyro_data = struct.unpack('4f', message_data[1])
-            print('gyro_data: ', gyro_data)
-        elif message_data[0] == 1:
-            if (len(message_data[1]) != 20):
-                reserved_data = msg_data
-                continue
-            gsensor_data = struct.unpack('5f', message_data[1])
-            print('gsensor_data: ', gsensor_data)
-        elif message_data[0] == 2:
-            if (len(message_data[1]) != 112):
-                reserved_data = msg_data
-                continue
-            print(len(message_data[1]))
-            gps_data = struct.unpack('=2i3f2d32s2f3i6f', message_data[1])
-            print('gps_data: ', gps_data)
-        else:
-            print("invalid socket data type")
-    
-    # c.send(b'server send data')
-    # c.close()
+   c, addr = s.accept()     # Establish connection with client.
+   _thread.start_new_thread(on_new_client,(c,addr))
+s.close()
